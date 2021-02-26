cf6453e82933eccc8a0bb200ab8bcd4d6a756afe
alishkb <ashokouhabdi@gmail.com>
2021-02-26 21:13:36 +0330

    change view of create new post

--- posts/forms.py
+++ posts/forms.py
@@ -6,6 +6,12 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text', 'category', 'tag')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'text': forms.Textarea(attrs={'class':'form-control'}),
+            'category': forms.Select(attrs={'class':'form-control'}),
+            'tag': forms.SelectMultiple(attrs={'class':'form-control'})
+        }
 
 class EditPostForm(forms.ModelForm):
     class Meta:
